a082fa8c6b84197019984347a4cd479ca38fef78
AndrewChan <34884018+AndrewChan2022@users.noreply.github.com>
2025-08-24 23:43:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ import sys
 import distutils.sysconfig
 from pathlib import Path
 
-########### debug:  pip install -v . 
+########### debug not:  pip install -v . 
+########### debug :  python setup.py install
 
 from setuptools import Extension, setup, find_packages
 from setuptools.command.build_ext import build_ext
